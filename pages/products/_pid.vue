faf9320fc0b0557628467c52834d8505e1084acf
Huy Do <thehuyvb@gmail.com>
2021-06-14 20:41:08 +0700

    finish assignment

---
+++ pages/products/_pid.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="container">
+        <h1>Product Detail Page with PID: {{ $route.params.pid }}</h1>
+        <br>
+        <nuxt-link to="/products">Back</nuxt-link>
+    </section>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="container">
+        <h1>Product Index Page</h1>
+        <ol>
+            <li>
+                <nuxt-link to="/products/1">Product 1</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/products/2">Product 2</nuxt-link>
+            </li>
+        </ol>
+    </section>
+</template>
+
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+.container ol li {
+    list-style-type: none;
+}
+</style>
